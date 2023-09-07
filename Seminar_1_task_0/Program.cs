dd40ea0ae70dde778993b2c6b24990b2df2b276e
Lonhop <egore_a@mail.ru>
2023-09-07 19:52:18 +0300

    solution from Seminar_1 _task_ 0,0.1,3,5.1

---
+++ Seminar_1_task_0/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число:  ");
+// считываем данные с консоли
+string? inputNum = Console.ReadLine();
+// проверяем чтобы не был пустым
+if(inputNum!=null)
+{
+    Console.WriteLine("Квадрат числа:"+(int)Math.Pow(int.Parse(inputNum),2));
+}
